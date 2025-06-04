c0c0ea9619f9a2ff7a6fa74f8016013a7464e411
Corey Hastings <stingzld@gmail.com>
2025-06-03 23:55:03 -0500

    Solution Files

--- src/test_htmlnode.py
+++ src/test_htmlnode.py
@@ -1,32 +1,32 @@
 import unittest
-from htmlnode import HTMLNode, LeafNode
+from htmlnode import LeafNode, ParentNode, HTMLNode
 
 
 class TestHTMLNode(unittest.TestCase):
     def test_to_html_props(self):
         node = HTMLNode(
-            "tag1",
-            props={"type": "quote", "source": "musical"},
+            "div",
+            "Hello, world!",
+            None,
+            {"class": "greeting", "href": "https://boot.dev"},
         )
         self.assertEqual(
             node.props_to_html(),
-            ' type="quote" source="musical"',
+            ' class="greeting" href="https://boot.dev"',
         )
 
     def test_values(self):
         node = HTMLNode(
-            "tag2",
-            "The internet is for porn",
-            None,
-            {"type": "quote", "source": "Avenue Q"},
+            "div",
+            "I wish I could read",
         )
         self.assertEqual(
             node.tag,
-            "tag2",
+            "div",
         )
         self.assertEqual(
             node.value,
-            "The internet is for porn",
+            "I wish I could read",
         )
         self.assertEqual(
             node.children,
@@ -34,19 +34,19 @@ class TestHTMLNode(unittest.TestCase):
         )
         self.assertEqual(
             node.props,
-            {"type": "quote", "source": "Avenue Q"},
+            None,
         )
 
     def test_repr(self):
         node = HTMLNode(
-            "tag3",
-            "A face so fair, a heart so pure - Sir, if you had been born a woman, you would have been she!",
+            "p",
+            "What a strange world",
             None,
-            {"type": "quote", "source": "A Funny Thing Happened on the Way to the Forum"},
+            {"class": "primary"},
         )
         self.assertEqual(
             node.__repr__(),
-            "HTMLNode(tag3, A face so fair, a heart so pure - Sir, if you had been born a woman, you would have been she!, None, {'type': 'quote', 'source': 'A Funny Thing Happened on the Way to the Forum'})",
+            "HTMLNode(p, What a strange world, children: None, {'class': 'primary'})",
         )
 
     def test_leaf_to_html_p(self):
@@ -63,6 +63,51 @@ class TestHTMLNode(unittest.TestCase):
     def test_leaf_to_html_no_tag(self):
         node = LeafNode(None, "Hello, world!")
         self.assertEqual(node.to_html(), "Hello, world!")
+
+    def test_to_html_with_children(self):
+        child_node = LeafNode("span", "child")
+        parent_node = ParentNode("div", [child_node])
+        self.assertEqual(parent_node.to_html(), "<div><span>child</span></div>")
+
+    def test_to_html_with_grandchildren(self):
+        grandchild_node = LeafNode("b", "grandchild")
+        child_node = ParentNode("span", [grandchild_node])
+        parent_node = ParentNode("div", [child_node])
+        self.assertEqual(
+            parent_node.to_html(),
+            "<div><span><b>grandchild</b></span></div>",
+        )
+
+    def test_to_html_many_children(self):
+        node = ParentNode(
+            "p",
+            [
+                LeafNode("b", "Bold text"),
+                LeafNode(None, "Normal text"),
+                LeafNode("i", "italic text"),
+                LeafNode(None, "Normal text"),
+            ],
+        )
+        self.assertEqual(
+            node.to_html(),
+            "<p><b>Bold text</b>Normal text<i>italic text</i>Normal text</p>",
+        )
+
+    def test_headings(self):
+        node = ParentNode(
+            "h2",
+            [
+                LeafNode("b", "Bold text"),
+                LeafNode(None, "Normal text"),
+                LeafNode("i", "italic text"),
+                LeafNode(None, "Normal text"),
+            ],
+        )
+        self.assertEqual(
+            node.to_html(),
+            "<h2><b>Bold text</b>Normal text<i>italic text</i>Normal text</h2>",
+        )
+
 
 if __name__ == "__main__":
     unittest.main()
